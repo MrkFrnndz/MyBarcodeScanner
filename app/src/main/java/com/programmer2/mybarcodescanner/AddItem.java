10c656a8a8ae9697e3e890423679538ff260acc3
MrkFrnndz <fernandezanthony244@gmail.com>
2017-05-02 11:44:43 +0800

    Initial commit

---
+++ app/src/main/java/com/programmer2/mybarcodescanner/AddItem.java
@@ -0,0 +1,9 @@
+package com.programmer2.mybarcodescanner;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by PROGRAMMER2 on 5/2/2017.
+ */
+public class AddItem extends AppCompatActivity{
+}
---
+++ app/src/main/java/com/programmer2/mybarcodescanner/Scan.java
@@ -0,0 +1,17 @@
+package com.programmer2.mybarcodescanner;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by PROGRAMMER2 on 5/2/2017.
+ */
+public class Scan extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+    }
+}
